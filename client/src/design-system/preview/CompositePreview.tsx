27c760b6ef35d3191005360149c2a5534633a501
Eng.Alhareth Al-Dahya <gcatfwg@gmail.com>
2026-09-24 22:59:07 +0300

    V3-03: bundle IBM Plex Arabic in visual harness [font-lock-generated]

--- client/src/design-system/preview/CompositePreview.tsx
+++ client/src/design-system/preview/CompositePreview.tsx
@@ -1,3 +1,7 @@
+import "@fontsource/ibm-plex-sans-arabic/400.css";
+import "@fontsource/ibm-plex-sans-arabic/500.css";
+import "@fontsource/ibm-plex-sans-arabic/600.css";
+import "@fontsource/ibm-plex-sans-arabic/700.css";
 import { useState } from "react";
 import { functionalIcons } from "../icons/icon-system";
 import { v3AssetPaths } from "../assets/asset-paths";
